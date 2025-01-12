54bc71f63f0231bf0a94fd89b268bd7c834c41bb
Dan Barton <danbarton2000@gmail.com>
2025-01-12 20:14:29 +0000

    Solved Step 3. Now counts adjacent lines. Did some refactoring including using the lines iterator to read the lines. Added some tests.

---
+++ src/lib.rs
@@ -0,0 +1,75 @@
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader, Write};
+use std::path::PathBuf;
+
+pub fn uniq<R: BufRead, W: Write>(input: R, mut output: W, count: bool) -> std::io::Result<()> {
+    let mut lines = input.lines();
+    let mut prev_line = None;
+    let mut current_count = 0;
+
+    let mut write = |line: Option<&String>, line_count: usize| -> std::io::Result<()> {
+        if let Some(line) = line {
+            if count {
+                writeln!(output, "{} {}", line_count, line)?;
+            } else {
+                writeln!(output, "{}", line)?;
+            }
+        }
+        Ok(())
+    };
+
+    while let Some(line) = lines.next() {
+        let line = line?;
+        if Some(&line) == prev_line.as_ref() {
+            current_count += 1;
+        } else {
+            write(prev_line.as_ref(), current_count)?;
+            prev_line = Some(line);
+            current_count = 1;
+        }
+    }
+
+    write(prev_line.as_ref(), current_count)?;
+
+    Ok(())
+}
+
+pub fn build_reader(path: Option<PathBuf>) -> std::io::Result<Box<dyn BufRead>> {
+    if let Some(path) = path {
+        if path.to_str() == Some("-") {
+            return Ok(Box::new(BufReader::new(stdin())));
+        }
+        File::open(&path)
+            .map(|file| Box::new(BufReader::new(file)) as Box<dyn BufRead>)
+    } else {
+        Ok(Box::new(BufReader::new(stdin())))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_uniq() {
+        let input = Cursor::new("apple\napple\nbanana\napple\nbanana\nbanana\n");
+        let mut output = Vec::new();
+
+        uniq(input, &mut output, false).unwrap();
+
+        let result = String::from_utf8(output).unwrap();
+        assert_eq!(result, "apple\nbanana\napple\nbanana\n");
+    }
+
+    #[test]
+    fn test_uniq_with_count() {
+        let input = Cursor::new("apple\napple\nbanana\napple\nbanana\nbanana\n");
+        let mut output = Vec::new();
+
+        uniq(input, &mut output, true).unwrap();
+
+        let result = String::from_utf8(output).unwrap();
+        assert_eq!(result, "2 apple\n1 banana\n1 apple\n2 banana\n");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,19 @@
-use std::fs::File;
-use std::hash::{DefaultHasher, Hash, Hasher};
-use std::io::{stdin, BufRead, BufReader};
+use std::io::stdout;
 use std::path::PathBuf;
 use clap::Parser;
+use cc_uniq_rs::{build_reader, uniq};
 
 fn main() {
     let args = Args::parse();
-    let mut reader = build_reader(args.input_path)
-        .expect("Failed to create reader.");
-
-    let mut line = String::new();
-    let mut last_hash = None;
-
-    while reader.read_line(&mut line).unwrap() > 0 {
-        let current_hash = calculate_hash(&line);
-        if let Some(last_hash) = last_hash {
-            if last_hash != current_hash {
-                print!("{line}");
-            }
-        } else {
-            print!("{line}")
-        }
-
-        last_hash = Some(current_hash);
-        line.clear();
-    }
-}
-
-fn build_reader(path: Option<PathBuf>) -> std::io::Result<Box<dyn BufRead>> {
-    if let Some(path) = path {
-        if path.to_str() == Some("-") {
-            return Ok(Box::new(BufReader::new(stdin())));
-        }
-        File::open(&path)
-            .map(|file| Box::new(BufReader::new(file)) as Box<dyn BufRead>)
-    } else {
-        Ok(Box::new(BufReader::new(stdin())))
-    }
-}
-
-// Taken from https://doc.rust-lang.org/std/hash/index.html
-fn calculate_hash<T: Hash>(t: &T) -> u64 {
-    let mut s = DefaultHasher::new();
-    t.hash(&mut s);
-    s.finish()
+    let reader = build_reader(args.input_path)
+        .expect("Failed to create reader");
+    uniq(reader, stdout(), args.count).unwrap();
 }
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    input_path: Option<PathBuf>
+    input_path: Option<PathBuf>,
+    #[clap(long, short, action)]
+    count: bool,
 }
